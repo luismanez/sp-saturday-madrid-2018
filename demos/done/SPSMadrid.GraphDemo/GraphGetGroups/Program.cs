8d119e7d3c84eb661b3cf15a1b74fcaca3f98124
luismanez <luis.manez@outlook.com>
2018-06-02 17:32:10 +0200

    added basic vs demo project structure

---
+++ demos/done/SPSMadrid.GraphDemo/GraphGetGroups/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GraphGetGroups
+{
+    /// <summary>
+    /// .NET Core 2.1 Console App showing how to call MS Graph getting Groups using App Permissions and MS Graph .Net SDK
+    /// Dependencies:
+    ///     Microsoft.Graph
+    ///     Microsoft.IdentityModel.Clients.ActiveDirectory
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
